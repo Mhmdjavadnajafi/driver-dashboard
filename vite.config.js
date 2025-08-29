841e49e715739104cfcf87b3f46239c82112ca8c
Muhammad Javad Najafi <93428582+Mhmdjavadnajafi@users.noreply.github.com>
2025-08-29 21:03:26 +0430

    add login

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
     proxy: {
       '/api': {
         target: 'https://api.tda24.ir',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
