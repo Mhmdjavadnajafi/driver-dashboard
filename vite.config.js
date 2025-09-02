af64effa695b5709e425271985c3585280f5b53c
Muhammad Javad Najafi <93428582+Mhmdjavadnajafi@users.noreply.github.com>
2025-09-02 10:53:51 +0430

    Add Netlify build config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.tda24.ir',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 })
